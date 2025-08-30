8e1656bc1c650f563b1e374eda12304ff485670b
LGP-Stalke <kilyan.ferrero@lesgeeksdupc.fr>
2025-08-30 12:12:55 +0200

    Create AuthGuard.tsx

---
+++ web/src/AuthGuard.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import { useLocation, Navigate } from "react-router-dom";
+import { useAuth } from "./auth";
+
+export default function AuthGuard({ children }: { children: ReactNode }) {
+  const { user } = useAuth();
+  const loc = useLocation();
+  if (!user) return <Navigate to="/login" replace state={{ from: loc }} />;
+  return <>{children}</>;
+}
